d18e7e80fd6b4314f6cc95b0223a8aa9a8a20b15
metjeanjunior <metjeanjunior@gmail.com>
2016-05-15 00:41:41 -0400

    more rm error cleanup & implemented workerinfo

--- rm/RMThread.java
+++ rm/RMThread.java
@@ -1,30 +1,40 @@
+import java.net.*;
+
 public class RMThread implements Runnable
 {
 	RMUtils rUtils;
-	Datagrampacket packet;
-	public RMThread(RMUtils rUtils, Datagrampacket packet)
+	DatagramPacket packet;
+	public RMThread(RMUtils rUtils, DatagramPacket packet)
 	{
 		this.rUtils = rUtils;
 		this.packet = packet;
 	}
 
-	public void run() throws Exception
+	public void run() 
 	{
 		System.out.println(Thread.currentThread().getName() + " started w/ id " + Thread.currentThread().getId());
 
-		String data = rUtils.getDataFromPacket(packet);
+		try 
+		{
+			String data = rUtils.getDataFromPacket(packet);
+			
+			if (data.compareTo("__server__") == 0)
+			{
+				rUtils.pushWorker(packet);
+			}
+			else // if (data.split(",")[0].compareTo("client"))
+			{
+				if(data.split(",")[0].compareTo("upload") == 0)
+					rUtils.sendRolePacket(data, "upl+");
+				else
+					rUtils.sendRolePacket(data, "dwl");
+			}
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 
-		if (data.compareTo("__server__") == 0)
-		{
-			rUtils.pushWorker(packet);
-		}
-		else // if (data.split(",")[0].compareTo("client"))
-		{
-			if(data.split(",")[0].compareTo("upload"))
-				rUtils.sendRolePacket(data, "upl+");
-			else
-				rUtils.sendToDownloader(data, "dwl");
-		}
 
 	}
 }
